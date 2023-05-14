23860cc38777fb64d355c854a2ad478bed552ed7
Elvira <adilovaelvira123@gmail.com>
2023-05-15 05:02:13 +0600

    put()

--- MyHashTable.java
+++ MyHashTable.java
@@ -4,7 +4,7 @@ public class MyHashTable<K, V> {
         private V value;
         private HashNode<K, V> next;
 
-        public HashNode(K key, V value){
+        public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
@@ -23,20 +23,36 @@ public class MyHashTable<K, V> {
         this.chainArray = new HashNode[M];
         this.size = 0;
     }
+
     public MyHashTable(int M) {
         this.chainArray = new HashNode[M];
         this.size = 0;
         this.M = M;
     }
-    private int hash(K key){
+
+    private int hash(K key) {
         int hashCode = key.hashCode();
-        int index = hashCode%M;
-        if (index>=0){
+        int index = hashCode % M;
+        if (index >= 0) {
             return index;
-        }
-        else{
-            return index+M;
+        } else {
+            return index + M;
         }
     }
 
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+    }
 }
