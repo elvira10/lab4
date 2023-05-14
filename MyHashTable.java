9f4f87d45601910d8ee916d1f83a3feb2e34527e
Elvira <adilovaelvira123@gmail.com>
2023-05-15 05:44:49 +0600

    contains()

--- MyHashTable.java
+++ MyHashTable.java
@@ -85,5 +85,17 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    public boolean contains(V value) {
+        for (int i = 0; i < chainArray.length; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
 
 }
