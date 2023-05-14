f979d95dd7fea231497cf79dac48c3adedbdbf95
Elvira <adilovaelvira123@gmail.com>
2023-05-15 05:32:08 +0600

    remove()

--- MyHashTable.java
+++ MyHashTable.java
@@ -66,5 +66,24 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> prev = null;
+        HashNode<K, V> curr = chainArray[index];
+        while (curr != null) {
+            if (curr.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = curr.next;
+                } else {
+                    prev.next = curr.next;
+                }
+                size--;
+                return curr.value;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return null;
+    }
 
 }
