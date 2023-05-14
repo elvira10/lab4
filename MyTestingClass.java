57c22c1e1b5fb04a9d42db5df7e2ba7b871d1273
Elvira <adilovaelvira123@gmail.com>
2023-05-15 05:59:59 +0600

    MyTestingClass start

---
+++ MyTestingClass.java
@@ -0,0 +1,11 @@
+public class MyTestingClass {
+    private int id;
+    private String name;
+    private int age;
+
+    public MyTestingClass(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
